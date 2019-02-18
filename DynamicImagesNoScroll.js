8f5a021ba039f9fd2ba2ad0c8cb9c8cf43cc8b5a
Tamir Gilany <tamirg@vision.bi>
2019-02-18 18:22:44 +0200

    first init

---
+++ DynamicImagesNoScroll.js
@@ -0,0 +1,297 @@
+"use strict";
+
+// Wrap everything in an anonymous function to avoid poluting the global namespace
+(() => {
+  const defaultIntervalInMin = "5";
+  let savedInfo;
+  let unregisterHandlerFunctions = [];
+
+  // Use the jQuery document ready signal to know when everything has been initialized
+  $(document).ready(() => {
+    // Tell Tableau we'd like to initialize our extension
+    tableau.extensions
+      .initializeAsync({ configure: configure })
+      .then(function() {
+        // This event allows for the parent extension and popup extension to keep their
+        // settings in sync.  This event will be triggered any time a setting is
+        // changed for this extension, in the parent or popup (i.e. when settings.saveAsync is called).
+        let currentSettings = tableau.extensions.settings.getAll();
+        fetchFilter();
+        fetchCurrentSettings();
+        if (typeof currentSettings.sheet !== "undefined") {
+          $("#inactive").hide();
+
+          //updateExtensionBasedOnSettings(currentSettings.newSettings);
+          parseInfo(currentSettings);
+        }
+
+        //console.log(savedSettingsInfo);
+        //console.log(settingsEvent);
+      });
+  });
+
+  /**
+   * Shows the choose sheet UI. Once a sheet is selected, the data table for the sheet is shown
+   */
+
+  let unregisterEventHandlerFunction;
+
+  function configure() {
+    const popupUrl = `${
+      window.location.origin
+    }/DynamicImagesNoScroll/extensionDialog.html`;
+
+    tableau.extensions.ui
+      .displayDialogAsync(popupUrl, defaultIntervalInMin, {
+        height: 500,
+        width: 500
+      })
+      .then(closePayload => {
+        $("#inactive").hide();
+        $("#active").show();
+
+        // The close payload is returned from the popup extension via the closeDialog method.
+      })
+      .catch(error => {
+        //  ...
+        // ... code for error handling
+      });
+  }
+
+  function getSelectedSheet(worksheetName) {
+    // go through all the worksheets in the dashboard and find the one we want
+    return tableau.extensions.dashboardContent.dashboard.worksheets.find(
+      function(sheet) {
+        return sheet.name === worksheetName;
+      }
+    );
+  }
+
+  function displayImages(images, count, percentages) {
+    let slideSize = 102;
+    $("#selected_marks").empty();
+
+    const navigation = $("<div>", {
+      class: "navigation"
+    }).appendTo("#selected_marks");
+
+    $("<div>", {
+      class: "leftArrow"
+    })
+      .click(() => {
+        $(".images").animate({ scrollLeft: "-=" + slideSize }, 500);
+      })
+      .appendTo(navigation);
+
+    const imagesContainer = $("<div>", {
+      class: "images"
+    }).appendTo(navigation);
+
+    for (let i = 0; i < images.length; i++) {
+      let imageContainer = $("<div>", {
+        class: "imageContainer"
+      }).appendTo("#selected_marks");
+
+      let image = images[i][0] + " ".split(",");
+      let singleCount = count[i][0] + " ".split(",");
+      let singlePercentages = percentages[i][0] + " ".split(",");
+
+      $("<img />", {
+        src: `${image}`,
+        alt: ""
+      }).appendTo(imageContainer);
+
+      $("<div>", {
+        class: "counter"
+      })
+        .text(`${singleCount}`)
+        .appendTo(imageContainer);
+
+      $("<div>", {
+        class: "percentages"
+      })
+        .text(`${singlePercentages}`)
+        .appendTo(imageContainer);
+
+      imagesContainer.append(imageContainer);
+    }
+
+    $("<div>", {
+      class: "rightArrow"
+    })
+      .click(() => {
+        $(".images").animate({ scrollLeft: "+=" + slideSize }, 500);
+      })
+      .appendTo(navigation);
+  }
+
+  function updateExtensionBasedOnSettings(settings) {
+    if (settings) {
+      savedInfo = settings;
+    }
+  }
+
+  // This letiable will save off the function we can call to unregister listening to marks-selected events
+  function initializeButtons() {
+    $("#show_choose_sheet_button").click(showChooseSheetDialog);
+  }
+
+  function getSelectedSheet(worksheetName) {
+    // go through all the worksheets in the dashboard and find the one we want
+    return tableau.extensions.dashboardContent.dashboard.worksheets.find(
+      function(sheet) {
+        return sheet.name === worksheetName;
+      }
+    );
+  }
+
+  function parseInfo(settings) {
+    if (unregisterEventHandlerFunction) {
+      unregisterEventHandlerFunction();
+    }
+
+    let worksheetsName = settings.sheet;
+    const worksheet = getSelectedSheet(worksheetsName);
+    unregisterEventHandlerFunction = worksheet.addEventListener(
+      tableau.TableauEventType.FilterChanged,
+      function(selectionEvent) {
+        parseInfo(settings);
+      }
+    );
+    let indexImage = settings.selectedImage[1];
+    let indexCount = settings.selectedCount[1];
+    let indexPercentages = settings.selectedPercentages[1];
+    // let cleanIndex = settings.selectedColumns.slice(
+    //   1,
+    //   settings.selectedColumns.length - 1
+    // );
+    // let indexColumnstable = cleanIndex.split(",");
+    // let columnsName = [];
+    // let columnsData = [];
+    worksheet.getSummaryDataAsync().then(marks => {
+      const worksheetData = marks;
+
+      // for (let i = 0; i < indexColumnstable.length; i++) {
+      //   let index = indexColumnstable[i];
+      //   //console.log(worksheetData.columns[index]);
+      //   columnsName.push(worksheetData.columns[index].fieldName);
+      // }
+
+      // const data = worksheetData.data.map(row => {
+      //   const rowData = row.map(cell => {
+      //     return cell.formattedValue;
+      //   });
+
+      //   for (let i = 0; i < indexColumnstable.length; i++) {
+      //     let index2 = indexColumnstable[i];
+      //     columnsData.push(rowData[index2]);
+      //   }
+      // });
+
+      const image = worksheetData.data.map(row => {
+        const rowData = row.map(cell => {
+          return cell.formattedValue;
+        });
+        return [rowData[indexImage]];
+      });
+
+      const count = worksheetData.data.map(row => {
+        const rowData = row.map(cell => {
+          return cell.formattedValue;
+        });
+        return [rowData[indexCount]];
+      });
+
+      const percentages = worksheetData.data.map(row => {
+        const rowData = row.map(cell => {
+          return cell.formattedValue;
+        });
+        return [rowData[indexPercentages]];
+      });
+
+      // Populate the data table with the rows and columns we just pulled out
+      displayImages(image, count, percentages);
+    });
+  }
+
+  function isOdd(num) {
+    return num % 2;
+  }
+
+  function fetchCurrentSettings() {
+    // While performing async task, show loading message to user.
+    //$('#loading').addClass('show');
+
+    // Whenever we restore the filters table, remove all save handling functions,
+    // since we add them back later in this function.
+    unregisterHandlerFunctions.forEach(unregisterHandlerFunction => {
+      unregisterHandlerFunction();
+    });
+
+    // Since filter info is attached to the worksheet, we will perform
+    // one async call per worksheet to get every filter used in this
+    // dashboard.  This demonstrates the use of Promise.all to combine
+    // promises together and wait for each of them to resolve.
+    let filterFetchPromises = [];
+
+    // List of all filters in a dashboard.
+    let dashboardfilters = [];
+
+    // To get filter info, first get the dashboard.
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+
+    tableau.extensions.settings.addEventListener(
+      tableau.TableauEventType.SettingsChanged,
+      settingsEvent => {
+        //console.log(settingsEvent);
+        //updateExtensionBasedOnSettings(settingsEvent.newSettings);
+        parseInfo(settingsEvent.newSettings);
+      }
+    );
+  }
+
+  function fetchFilter() {
+    // While performing async task, show loading message to user.
+    //$('#loading').addClass('show');
+
+    // Whenever we restore the filters table, remove all save handling functions,
+    // since we add them back later in this function.
+    unregisterHandlerFunctions.forEach(unregisterHandlerFunction => {
+      unregisterHandlerFunction();
+    });
+
+    // Since filter info is attached to the worksheet, we will perform
+    // one async call per worksheet to get every filter used in this
+    // dashboard.  This demonstrates the use of Promise.all to combine
+    // promises together and wait for each of them to resolve.
+    let filterFetchPromises = [];
+
+    // List of all filters in a dashboard.
+    let dashboardfilters = [];
+
+    // To get filter info, first get the dashboard.
+    const dashboard = tableau.extensions.dashboardContent.dashboard;
+
+    // Then loop through each worksheet and get its filters, save promise for later.
+    dashboard.worksheets.forEach(worksheet => {
+      //filterFetchPromises.push(worksheet.getFiltersAsync());
+
+      // Add filter event to each worksheet.  AddEventListener returns a function that will
+      // remove the event listener when called.
+      let unregisterHandlerFunction = worksheet.addEventListener(
+        tableau.TableauEventType.FilterChanged,
+        filterChangedHandler
+      );
+      //unregisterHandlerFunctions.push(unregisterHandlerFunction);
+    });
+  }
+
+  function filterChangedHandler(filterEvent) {
+    // Just reconstruct the filters table whenever a filter changes.
+    // This could be optimized to add/remove only the different filters.
+    //fetchFilters();
+    //reload gauge
+    const settingsSaved = tableau.extensions.settings.getAll();
+    parseInfo(settingsSaved);
+  }
+})();
---
+++ extensionDialog.js
@@ -0,0 +1,214 @@
+(() => {
+  /**
+   * This extension collects the IDs of each datasource the user is interested in
+   * and stores this information in settings when the popup is closed.
+   */
+  const imageSettingsKey = "selectedImage";
+  const countSettingsKey = "selectedCount";
+  const percentagesSettingsKey = "selectedPercentages";
+
+  let selectedImage = [];
+  let selectedCount = [];
+  let selectedPercentages = [];
+
+  $(document).ready(() => {
+    // The only difference between an extension in a dashboard and an extension
+    // running in a popup is that the popup extension must use the method
+    // initializeDialogAsync instead of initializeAsync for initialization.
+    // This has no affect on the development of the extension but is used internally.
+    tableau.extensions.initializeDialogAsync().then(openPayload => {
+      // The openPayload sent from the parent extension in this sample is the
+      // default time interval for the refreshes.  This could alternatively be stored
+      // in settings, but is used in this sample to demonstrate open and close payloads.
+      $("#interval").val(openPayload);
+      $("#closeButton").click(closeDialog);
+
+      let dashboard = tableau.extensions.dashboardContent.dashboard;
+
+      // Loop through datasources in this sheet and create a checkbox UI
+      // element for each one.  The existing settings are used to
+      // determine whether a datasource is checked by default or not.
+      dashboard.worksheets.forEach(worksheet => {
+        const button = createButton(worksheet.name);
+        button.click(() => {
+          $("#images").empty();
+          $("#count").empty();
+          $("#percentages").empty();
+
+          // Get the worksheet name which was selected
+          let worksheetName = worksheet.name;
+          tableau.extensions.settings.set("sheet", worksheetName);
+
+          // Close the dialog and show the data table for this worksheet
+          showChooseSelection(worksheetName);
+        });
+
+        $("#buttons").append(button);
+      });
+    });
+  });
+
+  /**
+   * Helper that parses the settings from the settings namesapce and
+   * returns a list of IDs of the datasources that were previously
+   * selected by the user.
+   */
+
+  function showChooseSelection(worksheetName) {
+    const worksheet = getSelectedSheet(worksheetName);
+
+    const textFormat = $(
+      "<h5>Select the field that indicated the URL of the image to display</h5>"
+    );
+    const textFormat2 = $("<h5>Select the count to display</h5>");
+    const textFormat3 = $("<h5>Select the percentages to display</h5>");
+
+    $("#images").append(textFormat);
+    $("#count").append(textFormat2);
+    $("#percentages").append(textFormat3);
+
+    worksheet.getSummaryDataAsync().then(data => {
+      const columnsTable = data.columns;
+      columnsTable.forEach(name => {
+        //addFieldItemToUI(name.fieldName);
+
+        const imageFieldOptions = createImageFieldOptions(name);
+        const countFieldOptions = createCountFieldOptions(name);
+        const percentagesFieldOptions = createPercentagesFieldOptions(name);
+      });
+    });
+  }
+
+  /**
+   * Helper that updates the internal storage of datasource IDs
+   * any time a datasource checkbox item is toggled.
+   */
+  function updateURL(id) {
+    let idIndex = selectedImage.indexOf(id);
+
+    if (idIndex < 0) {
+      selectedImage.push(id);
+    } else {
+      selectedImage.splice(idIndex, 1);
+    }
+  }
+
+  function updateCount(id) {
+    let idIndex = selectedCount.indexOf(id);
+
+    if (idIndex < 0) {
+      selectedCount.push(id);
+    } else {
+      selectedCount.splice(idIndex, 1);
+    }
+  }
+
+  function updatePercentages(id) {
+    let idIndex = selectedPercentages.indexOf(id);
+    if (idIndex < 0) {
+      selectedPercentages.push(id);
+    } else {
+      selectedPercentages.splice(idIndex, 1);
+    }
+  }
+
+  function closeDialog() {
+    let currentSettings = tableau.extensions.settings.getAll();
+    tableau.extensions.settings.set(
+      imageSettingsKey,
+      JSON.stringify(selectedImage)
+    );
+    tableau.extensions.settings.set(
+      countSettingsKey,
+      JSON.stringify(selectedCount)
+    );
+
+    tableau.extensions.settings.set(
+      percentagesSettingsKey,
+      JSON.stringify(selectedPercentages)
+    );
+
+    tableau.extensions.settings.saveAsync().then(newSavedSettings => {
+      tableau.extensions.ui.closeDialog("config");
+    });
+  }
+
+  function createButton(buttonTitle) {
+    const button = $(`<button type='button' class='btn btn-default btn-block'>
+        ${buttonTitle}
+      </button>`);
+    return button;
+  }
+
+  function createImageFieldOptions(buttonTitle) {
+    let containerDiv = $("<div />");
+
+    $("<input />", {
+      type: "radio",
+      id: buttonTitle.index,
+      name: "url_field",
+      value: buttonTitle.fieldName,
+      click: () => {
+        updateURL(buttonTitle.index);
+      }
+    }).appendTo(containerDiv);
+
+    $("<label />", {
+      for: buttonTitle.index,
+      text: buttonTitle.fieldName
+    }).appendTo(containerDiv);
+
+    $("#images").append(containerDiv);
+  }
+
+  function createCountFieldOptions(buttonTitle) {
+    let containerDiv = $("<div />");
+
+    $("<input />", {
+      type: "radio",
+      id: buttonTitle.index,
+      name: "counter_field",
+      value: buttonTitle.fieldName,
+      click: () => {
+        updateCount(buttonTitle.index);
+      }
+    }).appendTo(containerDiv);
+
+    $("<label />", {
+      for: buttonTitle.index,
+      text: buttonTitle.fieldName
+    }).appendTo(containerDiv);
+
+    $("#count").append(containerDiv);
+  }
+
+  function createPercentagesFieldOptions(buttonTitle) {
+    let containerDiv = $("<div />");
+
+    $("<input />", {
+      type: "radio",
+      id: buttonTitle.index,
+      name: "percentages_field",
+      value: buttonTitle.fieldName,
+      click: () => {
+        updatePercentages(buttonTitle.index);
+      }
+    }).appendTo(containerDiv);
+
+    $("<label />", {
+      for: buttonTitle.index,
+      text: buttonTitle.fieldName
+    }).appendTo(containerDiv);
+
+    $("#percentages").append(containerDiv);
+  }
+
+  function getSelectedSheet(worksheetName) {
+    // go through all the worksheets in the dashboard and find the one we want
+    return tableau.extensions.dashboardContent.dashboard.worksheets.find(
+      sheet => {
+        return sheet.name === worksheetName;
+      }
+    );
+  }
+})();
